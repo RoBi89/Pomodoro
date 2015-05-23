bf24b9df2c5b7b353111d49164c1ccf49db5d08a
robert <robi@posteo.de>
2015-05-23 18:19:44 +0200

    Renamed to pymodoro, notifications fully implemented

--- notifications.py
+++ notifications.py
@@ -4,11 +4,9 @@ from gi.repository import Notify
 import messages
 import endTimes
 
+Notify.init("Hello World")
 
-Notify.init("Hello world")
 
 workStartNotification = Notify.Notification.new((messages.workMessage), (endTimes.workTimeMessage), "dialog-information")
-
 shortPauseStartNotification = Notify.Notification.new((messages.workMessage), (endTimes.workTimeMessage), "dialog-information")
-
 longPauseStartNotification = Notify.Notification.new((messages.workMessage), (endTimes.workTimeMessage), "dialog-information")
