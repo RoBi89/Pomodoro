d3e1ed3433d53cea741c0ec695019951b1ad8766
Robert Bieber <robi@posteo.de>
2015-07-16 11:01:05 +0200

    started fixing a bug which causes the times shown to the user to not reflect changed values

--- variables.py
+++ variables.py
@@ -5,3 +5,6 @@ workDuration = 25  # Duration of a work phase, can be changed by the user
 shortPauseDuration = 5  # Duration of a short pause, can be changed by the user
 longPauseDuration = 20  # Duration of a long pause, can be changed by the user
 shortPauseCounter = 0  # Counts how many short pauses have already occured since the last long pause
+
+workDuration(x):
+    if 
